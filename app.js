cf244435371a02b394f90622aa4451c3d0c18e40
jberman <jarretberman@gmail.com>
2018-11-12 00:25:28 -0600

    animations

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ makeButton("mario")
 
 $("#clearBtns").on("click", function(event){
     event.preventDefault();
+    $(".hideThis").addClass("remove")
+    $(".gifBtn").addClass("remove")
+    $(".remove").animate({opacity:0}, "slow",null, function(){
+        $("#animals-view").empty()
+        $("#buttonDiv").empty()
+    })
+    
 
-    $("#animals-view").empty()
-    $("#buttonDiv").empty()
+    
     topicsArray = []
 })
 
@@ -20,46 +26,17 @@ $("#add-animal").on("click", function (event) {
     makeButton(input)
     $("#animals-view").empty()
 
-    $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?&q=" + input + "&api_key=dc6zaTOxFJmzC&limit=10",
-        method: "GET"
-
-    }).then(function (response) {
-        console.log(response)
-        for (i = 0; i < 10; i++) {
-            var imageUrl = response.data[i].images.fixed_height_still.url
-            var newGif = $("<img>")
-            $("#animals-view").prepend(newGif).prepend($("<p>").addClass("asap").text("Rating: " + response.data[i].rating)).prepend("<br>")
-            $(newGif).attr("src", imageUrl)
-            $(newGif).attr("data_still", response.data[i].images.fixed_height_still.url)
-            $(newGif).attr("data_animate", response.data[i].images.fixed_height.url)
-            $(newGif).attr("data_state", "still")
-
-
-            $(newGif).on("click", function () {
-                var animate = $(this).attr("data_animate")
-                var still = $(this).attr("data_still")
-                var source = $(this).attr("data_state")
-                if (source === "still") {
-                    $(this).attr("src", animate)
-                    $(this).attr("data_state", "animate")
-                    //console.log ("source", source)
-                    return
-                }
-                if (source === "animate") {
-                    //console.log ("its working")
-                    $(this).attr("src", still)
-                    $(this).attr("data_state", "still")
-                    return
-                }
-
-            })
-
-        }
-
-    })
+    ajaxx(input)
     $("#animal-input").val("")
 })
+
+
+
+
+
+
+
+
 
 
 function makeButton(input) {
@@ -87,43 +64,57 @@ function makeButton(input) {
     $(animal).on("click", function () {
         $("#animals-view").empty()
         var key = $(this).attr("data-animal")
-        $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?&q=" + key + "&api_key=dc6zaTOxFJmzC&limit=10",
-            method: "GET"
-
-        }).then(function (response) {
-            console.log(response)
-            for (i = 0; i < 10; i++) {
-                var imageUrl = response.data[i].images.fixed_height_still.url
-                var newGif = $("<img>")
-                $("#animals-view").prepend(newGif).prepend($("<p>").addClass("asap").text("Rating: " + response.data[i].rating)).prepend("<br>")
-                $(newGif).attr("src", imageUrl)
-                $(newGif).attr("data_still", response.data[i].images.fixed_height_still.url)
-                $(newGif).attr("data_animate", response.data[i].images.fixed_height.url)
-                $(newGif).attr("data_state", "still")
+        ajaxx(key)
+        
+    })
+}
 
 
-                $(newGif).on("click", function () {
-                    var animate = $(this).attr("data_animate")
-                    var still = $(this).attr("data_still")
-                    var source = $(this).attr("data_state")
-                    if (source === "still") {
-                        $(this).attr("src", animate)
-                        $(this).attr("data_state", "animate")
-                        //console.log ("source", source)
-                        return
-                    }
-                    if (source === "animate") {
-                        //console.log ("its working")
-                        $(this).attr("src", still)
-                        $(this).attr("data_state", "still")
-                        return
-                    }
 
-                })
 
-            }
 
-        })
+
+
+
+function ajaxx(key){
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?&q=" + key + "&api_key=dc6zaTOxFJmzC&limit=10",
+        method: "GET"
+
+    }).then(function (response) {
+        console.log(response)
+        for (i = 0; i < 10; i++) {
+            var imageUrl = response.data[i].images.fixed_height_still.url
+            var newGif = $("<img>")
+            $("#animals-view").prepend(newGif).prepend($("<p>").addClass("asap hideThis").text("Rating: " + response.data[i].rating)).prepend("<br>")
+            $(newGif).addClass("hideThis")
+            $(newGif).attr("src", imageUrl)
+            $(newGif).attr("data_still", response.data[i].images.fixed_height_still.url)
+            $(newGif).attr("data_animate", response.data[i].images.fixed_height.url)
+            $(newGif).attr("data_state", "still")
+
+
+            $(newGif).on("click", function () {
+                var animate = $(this).attr("data_animate")
+                var still = $(this).attr("data_still")
+                var source = $(this).attr("data_state")
+                if (source === "still") {
+                    $(this).attr("src", animate)
+                    $(this).attr("data_state", "animate")
+                    //console.log ("source", source)
+                    return
+                }
+                if (source === "animate") {
+                    //console.log ("its working")
+                    $(this).attr("src", still)
+                    $(this).attr("data_state", "still")
+                    return
+                }
+
+            })
+
+        }
+        $(".hideThis").animate({opacity:1},"slow")
+
     })
 }
