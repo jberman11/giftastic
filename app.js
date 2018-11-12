d3b34f9f461af8b85c74d22e1a9336d96e657659
jberman <jarretberman@gmail.com>
2018-11-11 23:21:32 -0600

    basic styling

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function makeButton(input){
 
         var animal = $("<button>")
 
-        $(animal).text(input)
+        $(animal).addClass("gifBtn").text(input.toUpperCase())
         $(animal).attr("data-animal",input)
         $("#buttonDiv").append(animal)
 
@@ -49,7 +49,7 @@ function makeButton(input){
                 for(i=0; i<10; i++){
                     var imageUrl = response.data[i].images.fixed_height_still.url
                     var newGif = $("<img>")
-                    $("#animals-view").prepend(newGif).prepend($("<p>").text("Rating: " +response.data[i].rating)).prepend("<br>")
+                    $("#animals-view").prepend(newGif).prepend($("<p>").addClass("asap").text("Rating: " +response.data[i].rating)).prepend("<br>")
                     $(newGif).attr("src",imageUrl)
                     $(newGif).attr("data_still", response.data[i].images.fixed_height_still.url)
                     $(newGif).attr("data_animate",response.data[i].images.fixed_height.url)
